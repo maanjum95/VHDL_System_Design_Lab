e1e8b5d88c53ef4657eb6f3b04f53736268c32d7
Anjum <ge46bod@prakt10.regent.e-technik.tu-muenchen.de>
2019-11-23 18:06:18 +0100

    Implemented and Tested Transformation Module.

---
+++ submit/direct/tb_trafo.vhd
@@ -0,0 +1,138 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   18:00:49 11/23/2019
+-- Design Name:   
+-- Module Name:   /nas/ei/share/TUEIEDA/LabHDL/2019w/ge46bod/submit/direct/tb_trafo.vhd
+-- Project Name:  idea
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: trafo
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_trafo IS
+END tb_trafo;
+ 
+ARCHITECTURE behavior OF tb_trafo IS 
+    -- Component Declaration for the Unit Under Test (UUT)
+    COMPONENT trafo
+    PORT(
+         X1 : IN  std_logic_vector(15 downto 0);
+         X2 : IN  std_logic_vector(15 downto 0);
+         X3 : IN  std_logic_vector(15 downto 0);
+         X4 : IN  std_logic_vector(15 downto 0);
+         Z1 : IN  std_logic_vector(15 downto 0);
+         Z2 : IN  std_logic_vector(15 downto 0);
+         Z3 : IN  std_logic_vector(15 downto 0);
+         Z4 : IN  std_logic_vector(15 downto 0);
+         Y1 : OUT  std_logic_vector(15 downto 0);
+         Y2 : OUT  std_logic_vector(15 downto 0);
+         Y3 : OUT  std_logic_vector(15 downto 0);
+         Y4 : OUT  std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+
+   --Inputs
+   signal X1 : std_logic_vector(15 downto 0) := (others => '0');
+   signal X2 : std_logic_vector(15 downto 0) := (others => '0');
+   signal X3 : std_logic_vector(15 downto 0) := (others => '0');
+   signal X4 : std_logic_vector(15 downto 0) := (others => '0');
+   signal Z1 : std_logic_vector(15 downto 0) := (others => '0');
+   signal Z2 : std_logic_vector(15 downto 0) := (others => '0');
+   signal Z3 : std_logic_vector(15 downto 0) := (others => '0');
+   signal Z4 : std_logic_vector(15 downto 0) := (others => '0');
+
+ 	--Outputs
+   signal Y1 : std_logic_vector(15 downto 0);
+   signal Y2 : std_logic_vector(15 downto 0);
+   signal Y3 : std_logic_vector(15 downto 0);
+   signal Y4 : std_logic_vector(15 downto 0);
+
+BEGIN
+	-- Instantiate the Unit Under Test (UUT)
+   uut: trafo PORT MAP (
+          X1 => X1,
+          X2 => X2,
+          X3 => X3,
+          X4 => X4,
+          Z1 => Z1,
+          Z2 => Z2,
+          Z3 => Z3,
+          Z4 => Z4,
+          Y1 => Y1,
+          Y2 => Y2,
+          Y3 => Y3,
+          Y4 => Y4
+        );
+
+	testing: process is
+	begin
+		-- 1
+		X1 <= x"0000";
+		X2 <= x"0000";
+		X3 <= x"0000";
+		X4 <= x"0000";
+		Z1 <= x"0000";
+		Z2 <= x"0000";
+		Z3 <= x"0000";
+		Z4 <= x"0000";
+		wait for 10ns;
+		
+		--2 
+		X1 <= x"ffff";
+		wait for 10ns;
+		
+		--3
+		X2 <= x"aaaa";
+		wait for 10ns;
+		
+		--4
+		X3 <= x"5555";
+		wait for 10ns;
+		
+		--5 
+		X4 <= x"2492";
+		wait for 10ns;
+		
+		--6
+		Z1 <= x"db6d";
+		wait for 10ns;
+		
+		--7
+		Z2 <= x"1c71";
+		wait for 10ns;
+		
+		--8
+		Z3 <= x"cccc";
+		wait for 10ns;
+		
+		--9
+		Z4 <= x"0002";
+		wait for 10ns;
+	
+	end process testing;
+ 
+
+END;
---
+++ submit/direct/trafo.vhd
@@ -0,0 +1,72 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    17:57:11 11/23/2019 
+-- Design Name: 
+-- Module Name:    trafo - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity trafo is
+    Port ( X1 : in  STD_LOGIC_VECTOR (15 downto 0);
+           X2 : in  STD_LOGIC_VECTOR (15 downto 0);
+           X3 : in  STD_LOGIC_VECTOR (15 downto 0);
+           X4 : in  STD_LOGIC_VECTOR (15 downto 0);
+           Z1 : in  STD_LOGIC_VECTOR (15 downto 0);
+           Z2 : in  STD_LOGIC_VECTOR (15 downto 0);
+           Z3 : in  STD_LOGIC_VECTOR (15 downto 0);
+           Z4 : in  STD_LOGIC_VECTOR (15 downto 0);
+           Y1 : out  STD_LOGIC_VECTOR (15 downto 0);
+           Y2 : out  STD_LOGIC_VECTOR (15 downto 0);
+           Y3 : out  STD_LOGIC_VECTOR (15 downto 0);
+           Y4 : out  STD_LOGIC_VECTOR (15 downto 0));
+end trafo;
+
+architecture Behavioral of trafo is
+    COMPONENT addop
+    PORT(
+         A : IN  std_logic_vector(15 downto 0);
+         B : IN  std_logic_vector(15 downto 0);
+         Y : OUT  std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+	  COMPONENT mulop
+    PORT(
+         I_1 : IN  std_logic_vector(15 downto 0);
+         I_2 : IN  std_logic_vector(15 downto 0);
+         O_1 : OUT  std_logic_vector(15 downto 0)
+        );
+    END COMPONENT;
+begin
+	M1: mulop PORT MAP(I_1=>X1, I_2=>Z1, O_1=>Y1);
+	
+	A1: addop PORT MAP(A=>X3, B=>Z2, Y=>Y2);
+	
+	A2: addop PORT MAP(A=>X2, B=>Z3, Y=>Y3);
+	
+	M2: mulop PORT MAP(I_1=>X4, I_2=>Z4, O_1=>Y4);
+
+end Behavioral;
+
