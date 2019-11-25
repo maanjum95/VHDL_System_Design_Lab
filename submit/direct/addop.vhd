3cd5d9f2bdf9854b1632719a12b7f0ceb664908a
Anjum <ge46bod@prakt6.regent.e-technik.tu-muenchen.de>
2019-11-25 16:45:15 +0100

    Refractor and Finalized Adder implementation.

--- submit/direct/addop.vhd
+++ submit/direct/addop.vhd
@@ -1,21 +1,9 @@
 ----------------------------------------------------------------------------------
--- Company: 
--- Engineer: 
+-- Engineer: 		 Muhammad A. Anjum
 -- 
 -- Create Date:    16:39:33 11/21/2019 
--- Design Name: 
 -- Module Name:    addop - Behavioral 
--- Project Name: 
--- Target Devices: 
--- Tool versions: 
--- Description: 
---
--- Dependencies: 
---
--- Revision: 
--- Revision 0.01 - File Created
--- Additional Comments: 
---
+-- Project Name: 	 IDEA Direct
 ----------------------------------------------------------------------------------
 library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
@@ -23,7 +11,7 @@ use IEEE.STD_LOGIC_UNSIGNED.ALL;
 
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---use IEEE.NUMERIC_STD.ALL;
+use IEEE.NUMERIC_STD.ALL;
 
 -- Uncomment the following library declaration if instantiating
 -- any Xilinx primitives in this code.
--- submit/direct/tb_addop.vhd
+++ submit/direct/tb_addop.vhd
@@ -1,45 +1,24 @@
 --------------------------------------------------------------------------------
--- Company: 
--- Engineer:
+-- Engineer:		Muhammad A. Anjum
 --
 -- Create Date:   16:44:56 11/21/2019
--- Design Name:   
 -- Module Name:   /nas/ei/share/TUEIEDA/LabHDL/2019w/ge46bod/submit/direct/tb_addop.vhd
--- Project Name:  idea
--- Target Device:  
--- Tool versions:  
--- Description:   
+-- Project Name:  IDEA Direct
 -- 
 -- VHDL Test Bench Created by ISE for module: addop
--- 
--- Dependencies:
--- 
--- Revision:
--- Revision 0.01 - File Created
--- Additional Comments:
---
--- Notes: 
--- This testbench has been automatically generated using types std_logic and
--- std_logic_vector for the ports of the unit under test.  Xilinx recommends
--- that these types always be used for the top-level I/O of a design in order
--- to guarantee that the testbench will bind correctly to the post-implementation 
--- simulation model.
 --------------------------------------------------------------------------------
 LIBRARY ieee;
 USE ieee.std_logic_1164.ALL;
-use IEEE.numeric_std.all;
  
 -- Uncomment the following library declaration if using
 -- arithmetic functions with Signed or Unsigned values
---USE ieee.numeric_std.ALL;
+USE ieee.numeric_std.ALL;
  
 ENTITY tb_addop IS
 END tb_addop;
  
 ARCHITECTURE behavior OF tb_addop IS 
- 
     -- Component Declaration for the Unit Under Test (UUT)
- 
     COMPONENT addop
     PORT(
          A : IN  std_logic_vector(15 downto 0);
@@ -47,7 +26,6 @@ ARCHITECTURE behavior OF tb_addop IS
          Y : OUT  std_logic_vector(15 downto 0)
         );
     END COMPONENT;
-    
 
    --Inputs
    signal A : std_logic_vector(15 downto 0) := (others => '0');
@@ -64,9 +42,8 @@ BEGIN
           Y => Y
         );
 	
-	-- Testing process for the adder over all possible boolean values
+	-- Testing process for the adder over booleans defined in test vector
 	testing: process is
-		constant P: integer := 2**16 - 1; -- upper limit for 16 bit
 	begin
 		-- 1
 		A <= x"0000";
@@ -94,5 +71,4 @@ BEGIN
 		wait for 10ns;
 	end process;
 	
-
 END;
